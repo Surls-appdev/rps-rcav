c27d1460001224cf01c87f7a4af792a1978e1dce
Matt surls <msurls@ChicagoBooth.edu>
2023-04-08 04:00:45 +0000

    Lots of progress 80.65% correct

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def homepage
+    render ({ :template => "game_templates/rules.html.erb"
+    })
+  end
+
 def play_rock
   #write a ton of code
 
@@ -12,5 +17,35 @@ def play_rock
   # render({ :html => "<h1>Hello, world!</h1>".html_safe})
   render({ :template => "game_templates/user_rock.html.erb"})
 end
+
+def play_paper
+  @comp_move = ["rock", "paper", "scissors"].sample
+  
+  if @comp_move == "paper" 
+    @outcome = "tied!"
+  elsif @comp_move == "scissors" 
+    @outcome = "lost!"
+  elsif @comp_move == "rock" 
+    @outcome = "won!"
+  end 
+
+  render({ :template => "game_templates/user_paper.html.erb"})
+
+end 
+
+def play_scissors
+  @comp_move = ["rock", "paper", "scissors"].sample
+  
+  if @comp_move == "paper" 
+    @outcome = "won!"
+  elsif @comp_move == "scissors" 
+    @outcome = "tied!"
+  elsif @comp_move == "rock" 
+    @outcome = "lost!"
+  end 
+
+  render({ :template => "game_templates/user_scissors.html.erb"})
+
+end 
 
 end
